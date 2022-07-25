98fefc460f097b69c457d4be4495244a94f67688
cimaron <cimaron713@gmail.com>
2022-07-25 21:24:50 +0900

    Guestbookproject

---
+++ README.md
@@ -0,0 +1,18 @@
+# GuestBook
+| Method | URI | Description |
+| --- | --- | --- |
+| Get | /guestbook/list | 리스트 조회 |
+| Post | /guestbook/register | 방명록 남기기 |
+| Get | /guestbook | redirect : /guestbook/list |
+| Post | /guestbook/remove | 방명록 삭제 |
+| Get | /guestbook/read | 방명록 읽기 |
+| Post | /guestbook/modify | 방명록 수정 |
+| get | /guestbook/list | 검색기능 |
+| Get | /guestbook/list(page= ${result.end + 1} | 다음 페이지 |
+| Get | /guestbook/list(page= ${result.start -1} | 이전 페이지 |
+
+test good
+test2
+test
+setset
+set
---
+++ src/test/java/org/zerock/guestbook/repository/GusetbookRepositoryTests.java
@@ -0,0 +1,47 @@
+package org.zerock.guestbook.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class GusetbookRepositoryTests {
+
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    public void testQuery1() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+
+        BooleanExpression exAll = exTitle.or(exContent); // 1
+
+        builder.and(exAll); //2
+
+        builder.and(qGuestbook.gno.gt(0L)); // 3
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+
+        }
+    }
